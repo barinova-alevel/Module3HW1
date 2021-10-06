a4ba83f8e4ed2c6528c265512a593cfea015965b
barinova-alevel <you@example.com>
2021-10-06 17:41:38 +0300

    Add project files.

---
+++ Generic_Implementation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generic_Implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
